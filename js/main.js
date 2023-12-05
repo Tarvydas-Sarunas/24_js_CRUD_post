a6904e98204a5753aec88bf6ad9121c672292dd0
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-05 09:21:59 +0100

    single post ir index su nav

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 "use strict";
 console.log("main.js file was loaded");
 
-const ulEl = document.getElementById("postList");
-
+const els = {
+  postListEl: document.getElementById("postList"),
+};
+console.log(els);
 const baseUrl = "http://localhost:5000";
 
 const postsUrl = `${baseUrl}/posts`;
@@ -25,27 +27,56 @@ async function getAllPosts() {
     console.warn(error);
   }
 }
-getAllPosts();
-// sugeneruoti postus htmle
-async function generateHTML() {
-  let postsUrl = await getAllPosts();
 
-  postsUrl.forEach((obj) => {
-    const newLi = document.createElement("li");
-    ulEl.append(newLi);
-    const cardDiv = document.createElement("div");
-    const cardBodyDiv = document.createElement("div");
-    newLi.append(cardDiv);
-    cardDiv.append(cardBodyDiv);
-    const h5El = document.createElement("h5");
-    h5El.textContent = obj.title;
-    const h6El = document.createElement("h6");
-    h6El.textContent = obj.author;
-    const pEl = document.createElement("p");
-    pEl.textContent = obj.body;
-    const aEl = document.createElement("a");
-    aEl.textContent = "Read More";
-    cardBodyDiv.append(h5El, h6El, pEl, aEl);
+function makePostsHtml(arr) {
+  arr.forEach((postObj) => {
+    // sukuriame viena html elementa
+    const singlePostEl = createSinglePosteEl(postObj);
+    // deti i sarasa
+    els.postListEl.append(singlePostEl);
   });
 }
-generateHTML();
+
+function createSinglePosteEl(singlePosteObj) {
+  const liEl = document.createElement("li");
+  const innerDiv = `
+  <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">${singlePosteObj.title}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary"> ${
+              singlePosteObj.author
+            } </h6>
+            <p class="card-text">${singlePosteObj.body.slice(0, 75)}... </p>
+            <a href="single-post.html" class="card-link btn btn-primary">Read more</a>
+
+          </div>
+        </div>
+  `;
+  liEl.innerHTML = innerDiv;
+  return liEl;
+}
+
+// sugeneruoti postus htmle
+
+// async function generateHTML() {
+//   let postsUrl = await getAllPosts();
+
+//   postsUrl.forEach((obj) => {
+//     const newLi = document.createElement("li");
+//     postListEl.append(newLi);
+//     const cardDiv = document.createElement("div");
+//     const cardBodyDiv = document.createElement("div");
+//     newLi.append(cardDiv);
+//     cardDiv.append(cardBodyDiv);
+//     const h5El = document.createElement("h5");
+//     h5El.textContent = obj.title;
+//     const h6El = document.createElement("h6");
+//     h6El.textContent = obj.author;
+//     const pEl = document.createElement("p");
+//     pEl.textContent = obj.body;
+//     const aEl = document.createElement("a");
+//     aEl.textContent = "Read More";
+//     cardBodyDiv.append(h5El, h6El, pEl, aEl);
+//   });
+// }
+// generateHTML();
